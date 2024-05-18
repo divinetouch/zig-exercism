c3864aa27e39da203087ff721cf49a897ec32c6f
Divine Touch <divine.touch07@gmail.com>
2024-05-17 22:12:07 -0500

    solved isogram

--- isogram/isogram.zig
+++ isogram/isogram.zig
@@ -1,4 +1,17 @@
+const std = @import("std");
+
 pub fn isIsogram(str: []const u8) bool {
-    _ = str;
-    @compileError("please implement the isIsogram function");
+    var list = std.AutoHashMap(u8, i32).init(std.heap.page_allocator);
+    defer list.deinit();
+
+    for (str) |c| {
+        const lower = std.ascii.toLower(c);
+        if (list.getKey(lower)) |_| {
+            return false;
+        } else if (std.ascii.isAlphabetic(c)) {
+            list.put(lower, 1) catch return false;
+        }
+    }
+
+    return true;
 }
