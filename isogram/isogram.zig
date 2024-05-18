f6cc42fe6792ba1bb8e9195c30fe3a28fb820afd
Divine Touch <divine.touch07@gmail.com>
2024-05-17 22:34:41 -0500

    minor refactoring

--- isogram/isogram.zig
+++ isogram/isogram.zig
@@ -1,7 +1,8 @@
 const std = @import("std");
 
 pub fn isIsogram(str: []const u8) bool {
-    var list = std.AutoHashMap(u8, i32).init(std.heap.page_allocator);
+    const allocator = std.heap.page_allocator;
+    var list = std.AutoHashMap(u8, i32).init(allocator);
     defer list.deinit();
 
     for (str) |c| {
